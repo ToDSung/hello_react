fdee7883b68d94278cf386d429e131ae109ec0c5
Tod Sung <wlunareve@gmail.com>
2021-10-20 18:07:04 +0800

    feat: add  mui component practice

--- src/components/title.js
+++ src/components/title.js
@@ -1,6 +1,36 @@
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import { Fragment } from 'react';
+
+const boxCount = 3
+const gridItems = [...Array(boxCount)].map((d ,i) => {
+  return (<Grid item bgcolor="primary.main" xs={4}>i</Grid>)
+})
+
+// Hidden 是根據視窗寬度隱藏元件的 component ，不是用 css 隱藏
+
 const title = (props) => {
-  console.log(123)
-  return <h1>{props.name}</h1>;
+  return (
+    <Fragment>
+      <Hidden smUp>
+        <Grid 
+          container
+          bgcolor="primary.main"
+        >
+          {
+            gridItems
+          }
+        </Grid>
+      </Hidden>
+      <Box
+        bgcolor="primary.main"
+        color="primary.contrastText"
+      >
+        {props.name}
+      </Box>
+    </Fragment>
+  )
 };
 
 export default title;
